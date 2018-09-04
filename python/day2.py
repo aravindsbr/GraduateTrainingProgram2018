90e5a6557d2c220cf6358bc6e8fffaddf0c6ce64
aravindsbr <aravindsbr@gmail.com>
2018-09-04 14:09:28 +0530

    Update day2.py

--- python/day2.py
+++ python/day2.py
@@ -29,19 +29,35 @@ print(inventory)
 """
 2)
 # qn 1
-student_details={'student1':[90,85, 80],
-				 'student2':[70,80,60]}
+#student_details={'student1':[90,85, 80],
+#			 'student2':[70,80,60]}
+student_details={}
+n=input("Enter number of students")
+s=input("Enter number of subjects")
+i=1
+
+
+while i<=n:
+	k=input("Enter key")
+	v=[]
+	j=1
+	while j<=s:
+		val=input("enter value for subject"+str(j))
+		v.append(val)
+		j=j+1
+		student_details[k]=v
+	i=i+1
+
 print(student_details)
 # qn 2
+
 total=0
 avg=0
-for k in student_details.iterkeys():
-	total=str(sum(student_details[k]))
-	print("Total of "+k+" is "+total)
+for a in student_details.iterkeys():
+	total=str(sum(student_details[a]))
+	print("Total of "+a+" is "+total)
 	total=int(total)
-	avg=total/len(student_details[k])
-	print("Average of "+k+" is "+str(avg))
+	avg=total/len(student_details[a])
+	print("Average of "+a+" is "+str(avg))
 """
-	
-	
 	
